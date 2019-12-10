ca1d083ce597a287d259e1479fbc9244f95bfbdc
Martín Nieva <martinjafactor@gmail.com>
2019-12-10 01:17:21 -0300

    Just improved the code, gotta fix a logic problem though

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from curses import textpad
 
 def main(stdscr):
     curses.raw()
+    stdscr.keypad(True)
+
+    file_name = "tf"
 
     height, width = stdscr.getmaxyx()
     beg = (4, 10)
@@ -20,46 +23,39 @@ def main(stdscr):
         key = stdscr.getch()
         cursor = stdscr.getyx()
 
-        if key == 3:
+        if key == 3: # User sends signal of interruption
+            if len(lines) == 0:
+                lines = [line + "\n"]
+
+            buf = "".join(lines)
+
+            with open(file_name, "w") as f:
+                f.write(buf)
+
             break
 
-        elif key == 10:
+        elif key == 10: # User presses enter
             if cursor[0] + 1 == end[0]:
                 continue
 
             lines.append(line + "\n")
             line = str()
             stdscr.move(cursor[0] + 1, beg[1] + 1)
-            continue
 
-        elif key == 263:
+        elif key == 263: # User presses backspace
             if cursor[1] - 1 == beg[1]:
                 continue
-                # if len(lines) == 0:
-                #     continue
-
-                # else:
-                #     stdscr.move(cursor[0] - 1, beg[1] + len(lines[-1]))
-                #     lines.pop()
-                #     continue
     
             stdscr.addstr(cursor[0], cursor[1] - 1, " ")
             stdscr.move(cursor[0], cursor[1] - 1)
             line = line[:-1]
-            continue
 
-        if len(line) + 1 == box_width:
-            continue
+        else: # User basically writes a character that's not backspace, enter nor Ctrl + C
+            if len(line) + 1 == box_width:
+                continue
 
-        line += chr(key)
-        stdscr.addstr(chr(key))
+            line += chr(key)
 
-    if len(lines) == 0:
-        lines = [line]
-
-    buf = "".join(lines)
-
-    with open("test_file", "w") as f:
-        f.write(buf)
+            stdscr.addstr(chr(key))
 
 curses.wrapper(main)
